7d73ac1942d878a0af4c5f4a7f5b0668581c03db
championshuttler <shivams2799@gmail.com>
2018-01-25 23:44:35 +0530

    child updaye

--- content_script.js
+++ content_script.js
@@ -7,7 +7,15 @@
         var chil, next;
         try {
             switch (node.nodeType) {
-
+                case 1: // elemeent
+                case 9: // document
+                case 11: // document fragment
+                    child = node.firstChild;
+                    while (child) {
+                        next = child.nextSibling;
+                        walk(child);
+                        child = next;
+                    }
             }
         }
     }
