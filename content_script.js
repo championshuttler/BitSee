82fa9d739d15e00f0e8317ab7d2ceb80a6acf001
championshuttler <shivams2799@gmail.com>
2018-01-25 23:40:09 +0530

    updaye

---
+++ content_script.js
@@ -0,0 +1,14 @@
+// Author : championshuttler
+// Date : 25th Jan 2018
+
+
+(function() {
+    function walk(node) {
+        var chil, next;
+        try {
+            switch (node.nodeType) {
+
+            }
+        }
+    }
+})
